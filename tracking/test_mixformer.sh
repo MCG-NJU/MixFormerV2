98e782c8dbbdd350b9b22f7e382de235244b249e
songtianhui <191098194@smail.nju.edu.cn>
2023-05-31 23:51:46 +0800

    support test

--- tracking/test_mixformer.sh
+++ tracking/test_mixformer.sh
@@ -3,7 +3,18 @@
 ### LaSOT test and evaluation
 python tracking/test.py mixformer2_vit_online 288_depth8_score \
  --dataset lasot --threads 32 --num_gpus 8 \
- --params__model models/mixformer2_base_b_online.pth.tar \
+ --params__model models/mixformerv2_base.pth.tar \
  --params__search_area_scale 5.0 \
  --debug 0
 # python tracking/analysis_results.py --dataset_name lasot --tracker_param 288_depth8_score
+
+
+##########-------------- MixFormerV2-Base-----------------##########
+
+### LaSOT test and evaluation
+# python tracking/test.py mixformer2_vit_online 224_depth4_mlp1_score \
+#  --dataset lasot --threads 32 --num_gpus 8 \
+#  --params__model models/mixformerv2_small.pth.tar \
+#  --params__search_area_scale 4.5 \
+#  --debug 0
+# python tracking/analysis_results.py --dataset_name lasot --tracker_param 224_depth4_mlp1_score
